f50584291f53b4ac548da831fc7268626c58222a
nijin26 <nijinazarkpz@gmail.com>
2022-06-12 21:19:07 +0530

    Add Contact Page

--- src/pages/Authentication/Authentication.jsx
+++ src/pages/Authentication/Authentication.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useForm, useToggle, upperFirst } from "@mantine/hooks";
-import { TextInput, PasswordInput, Text, Paper, Group, Stack, Button, Divider, Checkbox, Anchor } from "@mantine/core";
+import { Container, TextInput, PasswordInput, Text, Paper, Group, Stack, Button, Divider, Checkbox, Anchor } from "@mantine/core";
 import { GoogleButton } from "../../images/GoogleIcon";
 import { Link } from "react-router-dom";
 
@@ -21,53 +21,55 @@ const Authentication = (props) => {
   });
 
   return (
-    <Paper radius="md" p="xl" withBorder {...props}>
-      <Text size="lg" weight={500}>
-        Welcome to Online Fuel Delivery, {type} with
-      </Text>
+    <Container size="sm" mt={30}>
+      <Paper radius="md" p="xl" withBorder {...props}>
+        <Text size="lg" weight={500}>
+          Welcome to Online Fuel Delivery, {type} with
+        </Text>
 
-      <Group grow mb="md" mt="md">
-        <GoogleButton radius="xl">Google</GoogleButton>
-      </Group>
-
-      <Divider label="Or continue with email" labelPosition="center" my="lg" />
-
-      <form onSubmit={form.onSubmit(() => {})}>
-        <Group direction="column" grow>
-          {type === "register" && <TextInput label="Name" placeholder="Your name" value={form.values.name} onChange={(event) => form.setFieldValue("name", event.currentTarget.value)} />}
-
-          <TextInput required label="Email" placeholder="hello@gmail.com" value={form.values.email} onChange={(event) => form.setFieldValue("email", event.currentTarget.value)} error={form.errors.email && "Invalid email"} />
-
-          <PasswordInput
-            required
-            label="Password"
-            placeholder="Your password"
-            value={form.values.password}
-            onChange={(event) => form.setFieldValue("password", event.currentTarget.value)}
-            error={form.errors.password && "Password should include at least 6 characters"}
-          />
-
-          {type === "register" && <Checkbox label="I accept terms and conditions" checked={form.values.terms} onChange={(event) => form.setFieldValue("terms", event.currentTarget.checked)} />}
+        <Group grow mb="md" mt="md">
+          <GoogleButton radius="xl">Google</GoogleButton>
         </Group>
 
-        <Group position="apart" mt="xl">
-          <Stack spacing="xs">
-            <Anchor component="button" type="button" color="gray" onClick={() => toggle()} size="xs">
-              {type === "register" ? "Already have an account? Login" : "Don't have an account? Register"}
-            </Anchor>
-            {type === "login" && (
-              <Link to="/forgotpassword">
-                <Anchor component="button" type="button" color="gray" size="xs">
-                  Forgot Password ?
-                </Anchor>
-              </Link>
-            )}
-          </Stack>
+        <Divider label="Or continue with email" labelPosition="center" my="lg" />
 
-          <Button type="submit">{upperFirst(type)}</Button>
-        </Group>
-      </form>
-    </Paper>
+        <form onSubmit={form.onSubmit(() => {})}>
+          <Group direction="column" grow>
+            {type === "register" && <TextInput label="Name" placeholder="Your name" value={form.values.name} onChange={(event) => form.setFieldValue("name", event.currentTarget.value)} />}
+
+            <TextInput required label="Email" placeholder="hello@gmail.com" value={form.values.email} onChange={(event) => form.setFieldValue("email", event.currentTarget.value)} error={form.errors.email && "Invalid email"} />
+
+            <PasswordInput
+              required
+              label="Password"
+              placeholder="Your password"
+              value={form.values.password}
+              onChange={(event) => form.setFieldValue("password", event.currentTarget.value)}
+              error={form.errors.password && "Password should include at least 6 characters"}
+            />
+
+            {type === "register" && <Checkbox label="I accept terms and conditions" checked={form.values.terms} onChange={(event) => form.setFieldValue("terms", event.currentTarget.checked)} />}
+          </Group>
+
+          <Group position="apart" mt="xl">
+            <Stack spacing="xs">
+              <Anchor component="button" type="button" color="gray" onClick={() => toggle()} size="xs">
+                {type === "register" ? "Already have an account? Login" : "Don't have an account? Register"}
+              </Anchor>
+              {type === "login" && (
+                <Link to="/forgotpassword">
+                  <Anchor component="button" type="button" color="gray" size="xs">
+                    Forgot Password ?
+                  </Anchor>
+                </Link>
+              )}
+            </Stack>
+
+            <Button type="submit">{upperFirst(type)}</Button>
+          </Group>
+        </form>
+      </Paper>
+    </Container>
   );
 };
 
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { Container, Paper, Text, TextInput, Textarea, Button, Group, SimpleGrid, createStyles } from "@mantine/core";
+import { ContactIconsList } from "../components/ContactIconsList";
+
+import { useStyles } from "../styles/ContactUs";
 
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  const { classes } = useStyles();
+
+  return (
+    <Container mt={30}>
+      <Paper shadow="md" radius="lg">
+        <div className={classes.wrapper}>
+          <div className={classes.contacts}>
+            <Text size="lg" weight={700} className={classes.title} sx={{ color: "#fff" }}>
+              Contact information
+            </Text>
+
+            <ContactIconsList variant="white" />
+          </div>
+
+          <form className={classes.form} onSubmit={(event) => event.preventDefault()}>
+            <Text size="lg" weight={700} className={classes.title}>
+              Get in touch
+            </Text>
+
+            <div className={classes.fields}>
+              <SimpleGrid cols={2} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
+                <TextInput label="Your name" placeholder="Your name" />
+                <TextInput label="Your email" placeholder="hello@fueldev" required />
+              </SimpleGrid>
+
+              <TextInput mt="md" label="Subject" placeholder="Subject" required />
+
+              <Textarea mt="md" label="Your message" placeholder="Please include all relevant information" minRows={3} />
+
+              <Group position="right" mt="md">
+                <Button type="submit" className={classes.control}>
+                  Send message
+                </Button>
+              </Group>
+            </div>
+          </form>
+        </div>
+      </Paper>
+    </Container>
+  );
 };
 
 export default ContactUs;
