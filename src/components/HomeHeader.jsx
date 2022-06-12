f0f80e1e866c60cedcffac4e8e37c61cde506c23
nijin26 <nijinazarkpz@gmail.com>
2022-06-12 22:51:23 +0530

    Update NavBar to responsive

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Header, Container, Group, Burger } from "@mantine/core";
+import { Header, Container, Group, Burger, Transition, Paper } from "@mantine/core";
 import { useBooleanToggle } from "@mantine/hooks";
 // import { MantineLogo } from "../../shared/MantineLogo";
 import { Link } from "react-router-dom";
@@ -7,6 +7,38 @@ import { Link } from "react-router-dom";
 import { useStyles } from "../styles/HomeHeader";
 
 import ThemeToggle from "./ThemeToggle";
+
+// const HomeHeader = ({ links }) => {
+//   const [opened, toggleOpened] = useBooleanToggle(false);
+//   const [active, setActive] = useState(links[0].link);
+//   const { classes, cx } = useStyles();
+
+//   const items = links.map((link) => (
+//     <Link
+//       to={`${link.link}`}
+//       className={cx(classes.link, { [classes.linkActive]: active === link.link })}
+//       onClick={() => {
+//         setActive(link.link);
+//       }}
+//     >
+//       {link.label}
+//     </Link>
+//   ));
+
+//   return (
+//     <Header height={60}>
+//       <Container className={classes.header}>
+//         <Group spacing={5} className={classes.links}>
+//           {items}
+//         </Group>
+//         <ThemeToggle />
+//         <Burger opened={opened} onClick={() => toggleOpened()} className={classes.burger} size="sm" />
+//       </Container>
+//     </Header>
+//   );
+// };
+
+// export default HomeHeader;
 
 const HomeHeader = ({ links }) => {
   const [opened, toggleOpened] = useBooleanToggle(false);
@@ -26,13 +58,20 @@ const HomeHeader = ({ links }) => {
   ));
 
   return (
-    <Header height={60}>
+    <Header height={60} mb={120} className={classes.root}>
       <Container className={classes.header}>
         <Group spacing={5} className={classes.links}>
           {items}
         </Group>
         <ThemeToggle />
         <Burger opened={opened} onClick={() => toggleOpened()} className={classes.burger} size="sm" />
+        <Transition transition="pop-top-right" duration={200} mounted={opened}>
+          {(styles) => (
+            <Paper className={classes.dropdown} withBorder style={styles}>
+              {items}
+            </Paper>
+          )}
+        </Transition>
       </Container>
     </Header>
   );
--- src/styles/HomeHeader.js
+++ src/styles/HomeHeader.js
@@ -1,6 +1,70 @@
 import { createStyles } from "@mantine/core";
 
+// export const useStyles = createStyles((theme) => ({
+//   header: {
+//     display: "flex",
+//     justifyContent: "space-between",
+//     alignItems: "center",
+//     height: "100%",
+//   },
+
+//   links: {
+//     [theme.fn.smallerThan("xs")]: {
+//       display: "none",
+//     },
+//   },
+
+//   burger: {
+//     [theme.fn.largerThan("xs")]: {
+//       display: "none",
+//     },
+//   },
+
+//   link: {
+//     display: "block",
+//     lineHeight: 1,
+//     padding: "8px 12px",
+//     borderRadius: theme.radius.sm,
+//     textDecoration: "none",
+//     color: theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.colors.gray[7],
+//     fontSize: theme.fontSizes.sm,
+//     fontWeight: 500,
+
+//     "&:hover": {
+//       backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
+//     },
+//   },
+
+//   linkActive: {
+//     "&, &:hover": {
+//       backgroundColor: theme.colorScheme === "dark" ? theme.fn.rgba(theme.colors[theme.primaryColor][9], 0.25) : theme.colors[theme.primaryColor][0],
+//       color: theme.colors[theme.primaryColor][theme.colorScheme === "dark" ? 3 : 7],
+//     },
+//   },
+// }));
+
 export const useStyles = createStyles((theme) => ({
+  root: {
+    position: "relative",
+    zIndex: 1,
+  },
+
+  dropdown: {
+    position: "absolute",
+    top: 60,
+    left: 0,
+    right: 0,
+    zIndex: 0,
+    borderTopRightRadius: 0,
+    borderTopLeftRadius: 0,
+    borderTopWidth: 0,
+    overflow: "hidden",
+
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
+
   header: {
     display: "flex",
     justifyContent: "space-between",
@@ -9,13 +73,13 @@ export const useStyles = createStyles((theme) => ({
   },
 
   links: {
-    [theme.fn.smallerThan("xs")]: {
+    [theme.fn.smallerThan("sm")]: {
       display: "none",
     },
   },
 
   burger: {
-    [theme.fn.largerThan("xs")]: {
+    [theme.fn.largerThan("sm")]: {
       display: "none",
     },
   },
@@ -32,6 +96,11 @@ export const useStyles = createStyles((theme) => ({
 
     "&:hover": {
       backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
+    },
+
+    [theme.fn.smallerThan("sm")]: {
+      borderRadius: 0,
+      padding: theme.spacing.md,
     },
   },
 
