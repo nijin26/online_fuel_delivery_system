aa947f25a6c209598453b6b455eafdb2d5402a5c
nijin26 <nijinazarkpz@gmail.com>
2022-06-13 00:30:47 +0530

    Add Email Banner & update Home Page

---
+++ src/components/EmailBanner.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Text, Title, TextInput, Button, Image } from "@mantine/core";
+import EmailBannerImage from "../images/EmailBannerImage.svg";
+
+import { useStyles } from "../styles/EmailBanner";
+
+export function EmailBanner() {
+  const { classes } = useStyles();
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.body}>
+        <Title className={classes.title}>Wait a minute...</Title>
+        <Text weight={500} size="lg" mb={5} className={classes.subTitle}>
+          Subscribe to our newsletter!
+        </Text>
+        <Text size="sm" color="dimmed">
+          You will never miss important product updates, latest news and community QA sessions. Our newsletter is once a week, every Sunday.
+        </Text>
+
+        <div className={classes.controls}>
+          <TextInput placeholder="Your email" classNames={{ input: classes.input, root: classes.inputWrapper }} />
+          <Button className={classes.control}>Subscribe</Button>
+        </div>
+      </div>
+      <Image src={EmailBannerImage} className={classes.image} />
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,44 @@
 import React from "react";
 import { Container, Title, Text, Button } from "@mantine/core";
+import { Link } from "react-router-dom";
 
 import { useStyles } from "../styles/Home";
+
+import { EmailBanner } from "../components/EmailBanner";
 
 const Home = () => {
   const { classes } = useStyles();
   return (
-    <div className={classes.root}>
-      <Container size="md">
-        <div className={classes.inner}>
-          <div className={classes.content}>
-            <Title className={classes.title}>
-              A{" "}
-              <Text component="span" inherit variant="gradient" gradient={{ from: "green", to: "lightgreen" }}>
-                fully featured
-              </Text>{" "}
-              Online Fuel Delivery App
-            </Title>
+    <>
+      <div className={classes.root}>
+        <Container size="md">
+          <div className={classes.inner}>
+            <div className={classes.content}>
+              <Title className={classes.title}>
+                A{" "}
+                <Text component="span" inherit variant="gradient" gradient={{ from: "green", to: "lightgreen" }}>
+                  fully featured
+                </Text>{" "}
+                Online Fuel Delivery App
+              </Title>
 
-            <Text className={classes.description} mt={30}>
-              Order fuel at any time, anywhere and get delivered on your location
-            </Text>
+              <Text className={classes.description} mt={30}>
+                Order fuel at any time, anywhere and get delivered on your location
+              </Text>
 
-            <Button variant="gradient" gradient={{ from: "darkgreen", to: "green" }} size="xl" className={classes.control} mt={40}>
-              Get started
-            </Button>
+              <Link to="/auth">
+                <Button variant="gradient" gradient={{ from: "darkgreen", to: "green" }} size="xl" className={classes.control} mt={40}>
+                  Get started
+                </Button>
+              </Link>
+            </div>
           </div>
-        </div>
+        </Container>
+      </div>
+      <Container mt={30}>
+        <EmailBanner />
       </Container>
-    </div>
+    </>
   );
 };
 
